588892bfba562b3fc0b3f0694b7a693cc9cc1fbc
MrScautHD <65916181+MrScautHD@users.noreply.github.com>
2025-12-29 22:03:28 +0100

    Fix UIView using wrong P/Invoke function to return frame

--- src/Veldrid.MetalBindings/UIView.cs
+++ src/Veldrid.MetalBindings/UIView.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Runtime.InteropServices;
 using static Veldrid.MetalBindings.ObjectiveCRuntime;
 
 namespace Veldrid.MetalBindings
@@ -11,6 +12,9 @@ namespace Veldrid.MetalBindings
 
         public CALayer layer => objc_msgSend<CALayer>(NativePtr, "layer"u8);
 
-        public CGRect frame => CGRect_objc_msgSend(NativePtr, "frame"u8);
+        public CGRect frame =>
+            RuntimeInformation.ProcessArchitecture == Architecture.Arm64
+                ? CGRect_objc_msgSend(NativePtr, "frame"u8)
+                : objc_msgSend_stret<CGRect>(NativePtr, "frame"u8);
     }
 }
